db5c0832b83b59b0bd6b0992421bffdb1960de40
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-09-26 20:36:48 -0300

    feat: alterado pag inicial com o nome do projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,17 +15,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Projeto Vale a Pena</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Clique no botão de contagem {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Sejam Bem vindos, que todos possam adquirir bastante conhecimento!
       </p>
     </div>
   )
